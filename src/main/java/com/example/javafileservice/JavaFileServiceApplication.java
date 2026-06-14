2b8301f919161331490a63e0abff18aaaa94771e
linxygoof123 <2253723772@qq.com>
2026-06-14 17:51:04 +0800

    java-文件服务器-项目初始

---
+++ src/main/java/com/example/javafileservice/JavaFileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.javafileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFileServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaFileServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javafileservice/JavaFileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javafileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFileServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
